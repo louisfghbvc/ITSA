d63050f810d014acdeaf8405570d4e5293708c68
尾玉 <43661591+louisfghbvc@users.noreply.github.com>
2020-03-14 00:44:01 +0800

    String

--- itsa74(done)/Problem 5  跳舞的小人.java
+++ itsa74(done)/Problem 5  跳舞的小人.java
@@ -4,25 +4,29 @@ import java.util.*;
 public class Main {    
     
     public static void main(String[] args) {    
-        Scanner scn = new Scanner(System.in);    
-        int n = Integer.parseInt(scn.nextLine());  
+        Scanner input = new Scanner(System.in);    
+        int n = Integer.parseInt(input.nextLine());  
         while(n-- != 0){  
-            String s[] = scn.nextLine().split(" ");  
+            String s[] = input.nextLine().split(" ");  
             StringBuilder str = new StringBuilder("");  
             int num = 0;  
-            for(int i = s.length-1; i >= 0; i--){  
+            for(int i = s.length - 1; i >= 0; i--){  
                 char c = s[i].toCharArray()[0];  
                 if((c - '0') <= 9)  
                     num = Integer.parseInt(s[i]);  
                 else{  
-                    char tot=(char)(c+num);  
-                    if(tot > 'Z')  
-                        tot = (char)( tot % 'Z' + 'A' - 1);  
-                    str.append(tot);  
+                    char t = (char)(c + num);
+                    if(t > 'Z')
+                          t = (char)(t % 'Z' + 'A' - 1);
+                    str.append(t);  
                 }  
             }  
             System.out.println(str.reverse());  
         }  
     }  
     /*...Java才給過G8 */  
-} 
+}
+/*
+1
+A B C 13 D E 2 X Y Z 20
+*/
