6ac07afd15c14bb143b8417d1d43611f9ce120e1
尾玉 <43661591+louisfghbvc@users.noreply.github.com>
2020-10-15 20:49:33 +0800

    Tricky, Number theory

--- itsa68(done)/itsa 68 P3/main.cpp
+++ itsa68(done)/itsa 68 P3/main.cpp
@@ -1,25 +1,37 @@
-#include <iostream>
-#include <bitset>
+// Very Trick !!!..
+// Number theory. check target bit is equal, or not go to that bit...
 
+#include <bits/stdc++.h>
+
+#define Fast cin.tie(0), ios::sync_with_stdio(0)
 using namespace std;
-typedef unsigned long long ull;
-int main()
-{
-    ull m, s, d, ts;
-    bitset<32> bt;
+
+int m, s, d;
+void dfs(int u, int i){
+    cout << " " << u;
+    if(u == d) return;
+    for(int k = 0, j = i-1; k < m; ++k, --j){
+        if(j < 0) j += m;
+        int v = u^(1<<j);
+        if((v&(1<<j)) != (d&(1<<j))) continue;
+        dfs(v, j);
+        break;
+    }
+}
+
+void solve(){
     while(cin >> m >> s >> d){
-
-        for(int i = m; i > 0; --i){
-            ts = s;
-            bt.reset();
-            do{
-                cout << ts;
-                bt[i] = 1;
-                ts
-            }while(ts != d);
-
-
+        for(int j = m-1; j >= 0; --j){
+            cout << s;
+            dfs(s^(1<<j), j);
+            cout << "\n";
         }
     }
+}
+
+int main()
+{
+    Fast;
+    solve();
     return 0;
 }
