024ba05730050f1e896a11bd9462acc955e18f48
尾玉 <43661591+louisfghbvc@users.noreply.github.com>
2020-11-27 18:13:25 +0800

    Math, Square.

--- itsa74(done)/Problem 4 擲骰子.cpp
+++ itsa74(done)/Problem 4 擲骰子.cpp
@@ -63,3 +63,70 @@ int main()
   
     return 0;  
 } 
+
+
+// Fun.
+#include <bits/stdc++.h>
+
+#define Fast cin.tie(0), ios::sync_with_stdio(0)
+#define louisfghbvc int t; cin >> t; while(t--)
+using namespace std;
+
+template<typename T> ostream& operator<<(ostream &os, const vector<T> &v) { os << '{'; string sep = ""; for(const auto &x : v) os << sep << x, sep = ", "; return os << '}'; }
+void dbg_out() { cerr << " end.\n"; }
+template<typename Head, typename... Tail> void dbg_out(Head H, Tail... T) { cerr << ' ' << H; dbg_out(T...); }
+
+int side(vector<int> &a, vector<int> &b){
+    return (a[0]-b[0])*(a[0]-b[0]) + (a[1]-b[1])*(a[1]-b[1]);
+}
+
+pair<bool, double> valid(vector<vector<int>> &p){
+    set<int> st = {
+        side(p[0], p[1]), side(p[0], p[2]), side(p[0], p[3]),
+        side(p[1], p[2]), side(p[1], p[3]), side(p[2], p[3])
+    };
+    if(st.count(0) || st.size() != 2) return{false, 0};
+    return {true, sqrt(*st.begin())*sqrt(*st.begin())};
+}
+
+void solve(){
+    #define area second
+    int n;
+    while(cin >> n){
+        vector<vector<int>> a, b;
+        for(int i = 0; i < 8; i += 2){
+            int x, y;
+            cin >> x >> y;
+            a.push_back({x * n, y * n});
+        }
+        for(int i = 0; i < 8; i += 2){
+            int x, y;
+            cin >> x >> y;
+            b.push_back({x * n, y * n});
+        }
+
+        auto a_ans = valid(a), b_ans = valid(b);
+        if(a_ans.first)
+            cout << "Yes " << (int)round(a_ans.area) << endl;
+        else
+            cout << "No\n";
+        if(b_ans.first)
+            cout << "Yes " << (int)round(b_ans.area) << endl;
+        else
+            cout << "No\n";
+        if(a_ans.area > b_ans.area)
+            cout << "A\n";
+        else if(a_ans.area < b_ans.area)
+            cout << "B\n";
+        else
+            cout << "Peace\n";
+    }
+}
+
+int main()
+{
+    //Fast;
+    //louisfghbvc
+        solve();
+    return 0;
+}
