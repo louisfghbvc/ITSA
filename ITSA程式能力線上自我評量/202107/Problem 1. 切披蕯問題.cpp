8033c2e049363619554fc08012a141f60c1f61a6
尾玉 <43661591+louisfghbvc@users.noreply.github.com>
2021-07-15 22:29:40 +0800

    Observe.

---
+++ ITSA程式能力線上自我評量/202107/Problem 1. 切披蕯問題.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>  
+ 
+#define Fast cin.tie(0), ios::sync_with_stdio(0)  
+#define louisfghbvc int t; cin >> t; for(int tt = 0; tt < t; ++tt)  
+using namespace std;  
+  
+/** 
+Read problem statement carefully 
+        
+**/  
+
+void solve(int n){  
+    cin >> n;
+    cout << (1 + n) * n / 2 + 1 << "\n";
+}  
+  
+int main()  
+{  
+    // Fast;
+    louisfghbvc
+        solve(1);
+    return 0;
+}
