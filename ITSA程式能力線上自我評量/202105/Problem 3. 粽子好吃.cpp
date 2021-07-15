882a50173f28aade75d945bc7b1f1f4d791aa466
尾玉 <43661591+louisfghbvc@users.noreply.github.com>
2021-07-15 21:41:35 +0800

    Clean Code.

--- ITSA程式能力線上自我評量/202105/Problem 3. 粽子好吃.cpp
+++ ITSA程式能力線上自我評量/202105/Problem 3. 粽子好吃.cpp
@@ -1,89 +1,73 @@
-#include <bits/stdc++.h>
+#include <bits/stdc++.h>  
+ 
+#define Fast cin.tie(0), ios::sync_with_stdio(0)  
+#define louisfghbvc int t; cin >> t; for(int tt = 0; tt < t; ++tt)  
+using namespace std;  
+  
+/** 
+Read problem statement carefully 
+6 
+1 6 
+3 5 
+10 2 
+8 10 
+3 
+1 6 
+3 10 
+6 9 
+10 20 
+11 2 
+12 10 
+**/  
 
-#define Fast cin.tie(0), ios::sync_with_stdio(0)
-#define louisfghbvc int t; cin >> t; for(int tt = 0; tt < t; ++tt)
-using namespace std;
+typedef pair<int, int> ii;
 
-/**
-Read problem statement carefully
-6
-1 6
-3 5
-10 2
-8 10
+void solve(int n){  
+    cin >> n;  
+    int a, b;  
 
-3
-1 6
-3 10
-6 9
-10 20
-11 2
-12 10
-
-**/
-
-struct people{
-    int enter = 0, cost = 0;
-    bool isWen = false;
-    bool operator<(people &p2) const{
-        return enter < p2.enter;
+    vector<ii> arr;  
+    while(cin >> a >> b){  
+        arr.push_back({a, b});  
     }
-};
 
-void solve(int n){
-    cin >> n;
-    int a, b;
-    vector<people> arr;
-    while(cin >> a >> b){
-        arr.push_back({a, b, 0});
-    }
-    arr.back().isWen = true;
+    sort(arr.begin(), arr.end());  
+  
+    vector<int> center(n); // save the time
+    
+    int mn, mni;
+    for(auto &[at, tt]: arr){
+        if(at >= a && tt != b) break;
 
-    sort(arr.begin(), arr.end());
+        for(int i = 0; i < n; ++i){
+            if(center[i] < at)
+                center[i] = 0;
+        }
 
-    vector<int> center(n);
-    vector<bool> vis(n);
-    int i = 0;
-    for(int time = 0; time <= 10000; ++time){
-        // clean center
-        for(int k = 0; k < n; ++k){
-            if(center[k] <= time){
-                vis[k] = 0;
-                center[k] = 0;
+        mn = INT_MAX, mni = -1;
+        for(int i = 0; i < n; ++i){
+            if(center[i] < mn){
+                mn = center[i];
+                mni = i;
             }
         }
 
-        while(i < arr.size() && arr[i].enter <= time){
-            bool ok = 0;
-            for(int k = 0; k < n; ++k){
-                if(!vis[k]){
-                    vis[k] = 1;
-                    if(arr[i].isWen){
-                        cout << "Serve at this counter: " << k+1 << "\n";
-                        cout << "Waiting time: " << time - arr[i].enter << "\n";
-                        return;
-                    }
-                    center[k] = time + arr[i].cost;
-                    i++;
-                    ok = 1;
-                    break;
-                }
-            }
-            if(!ok) break;
-        }
-
+        // minimum must be accept
+        center[mni] = max(center[mni], at) + tt; 
     }
-
-}
-
-int main()
-{
-    // Fast;
-    // louisfghbvc
-        solve(1);
-    return 0;
-}
-
-/**
-Enjoy the problem.
-**/
+    
+    cout << "Serve at this counter: " << mni+1 << "\n";
+    cout << "Waiting time: " << max(0, mn - a) << "\n";
+}  
+  
+int main()  
+{  
+    // Fast;  
+    // louisfghbvc  
+        solve(1);  
+    return 0;  
+}  
+  
+/** 
+Enjoy the problem. 
+**/  
